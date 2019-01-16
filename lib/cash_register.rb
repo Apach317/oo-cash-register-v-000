8339474ef70339c082d720ae7fbb8c46e623ad66
Ron Lynch <lynch.ronald1@gmail.com>
2019-01-16 04:28:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+class CashRegister
+  attr_accessor :total, :discount, :price, :title, :last_trans
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(total, quantity)
+
+    if quantity = 0
+      @total += price
+    end
+
+    if quantity > 1
+      items = 0
+      while quantity > items
+        @cart << title
+        items =+ 1
+      end
+      else
+        @cart << title
+    end
+
+    @total += price * quantity
+    @last_trans = @total
+    @total
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+
+  end
+
+end
